0f8e7513b917517582c8fcb7e61d81a2ac9e6793
farleybsd <farley.t.i@hotmail.com>
2019-12-02 17:59:19 -0300

    cont

--- src/js/objectRestSpread.js
+++ src/js/objectRestSpread.js
@@ -5,3 +5,5 @@ console.log('frameworks:',frameworks)
 
 const novosCursos = {angular,...frameworks}
 console.log('novosCursos:',novosCursos)
+
+//https://cursos.alura.com.br/course/ecmascript-novidades/task/62994
