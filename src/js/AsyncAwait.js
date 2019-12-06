e3067cbc0ce0b112c0f2a74cbf5407f25d75f970
Farley de Souza R <Farley de Souza R@DESKTOP-CN59BUT>
2019-12-05 21:58:40 -0300

    Trantativa de erros do Async Await

--- src/js/AsyncAwait.js
+++ src/js/AsyncAwait.js
@@ -1,12 +1,21 @@
-(async   function asyncListaLivros(){
+(async function asyncListaLivros() {
+    try {
 
-  const resposta = await  fetch('https://www.googleapis.com/books/v1/volumes?q=javascript')
-  const dados =  await resposta.json()
- 
-  const items = dados.items.map(item => item)
-  const informacoes = items.map(item => item.volumeInfo)
-  const titulos = informacoes.map(info => info.title )
-  redenrizarLista("asyncListaLivro",titulos)
-  
+        const resposta = await fetch('https://www.googleapis.com/books/v1/volumes?q=javascript')
+        const dados = await resposta.json()
+
+        const items = dados.items.map(item => item)
+        const informacoes = items.map(item => item.volumeInfo)
+        const titulos = informacoes.map(info => info.title)
+        redenrizarLista("asyncListaLivro", titulos)
+
+
+    } catch (erro) {
+        console.error('Sai Correno' + erro)
+    } finally {
+        console.log('A funcao asynAwayt esta Ok!')
+    }
+
+
 })()
 
